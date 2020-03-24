d0fb6ce16c7dfc02cde5c69a3fdad7e5d15de323
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-03-24 13:44:25 +0530

    Working on Plotter utility

---
+++ src/utils/Plotter.ts
@@ -0,0 +1,15 @@
+
+// Imports
+import { Vector } from './Vector'
+
+// Interface for Plotter class
+export interface IPlotter {
+	
+}
+
+// Plotter Class
+export class Plotter implements IPlotter {
+
+	//
+
+}
