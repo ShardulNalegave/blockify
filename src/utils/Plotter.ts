62ebcd6b1f4ffb8d48390240dbdb588fd39fa8e0
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-03-24 14:25:46 +0530

    Interface for Plotter

--- src/utils/Plotter.ts
+++ src/utils/Plotter.ts
@@ -4,12 +4,31 @@ import { Vector } from './Vector'
 
 // Interface for Plotter class
 export interface IPlotter {
-	
+
+	withOrigin(pos: Vector): void
+
+	/**
+	 * Shapes
+	 */
+
+	triangle(a: Vector, b: Vector, c: Vector): void
+	rectangle(corner: Vector, scale: Vector): void
+
 }
 
 // Plotter Class
-export class Plotter implements IPlotter {
+@staticImplements<IPlotter>()
+export class Plotter {
 
-	//
+	public static withOrigin(pos: Vector) {}
 
+	public static triangle(a: Vector, b: Vector, c: Vector) {}
+
+	public static rectangle(corner: Vector, scale: Vector) {}
+
+}
+
+// Extras
+function staticImplements<T>() {
+	return <U extends T>(constructor: U) => {constructor};
 }
