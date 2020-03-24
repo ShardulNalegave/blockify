56d5c8b1bdbbce6a0596a3a7a1fa3bda0324ecdd
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-03-24 17:37:14 +0530

    Basic Colors implementation. Working on adding all colors.

--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -39,7 +39,16 @@ export class Color implements IColor {
 namespace Colors {
 
 	export class Red {
-		public static 100: Color = new Color("")
+		public static 50: Color = new Color("#FFEBEE")
+		public static 100: Color = new Color("#FFCDD2")
+		public static 200: Color = new Color("#EF9A9A")
+		public static 300: Color = new Color("#E57373")
+		public static 400: Color = new Color("#EF5350")
+		public static 500: Color = new Color("#F44336")
+		public static 600: Color = new Color("#E53935")
+		public static 700: Color = new Color("#D32F2F")
+		public static 800: Color = new Color("#C62828")
+		public static 900: Color = new Color("#B71C1C")
 	}
 
 }
