353c379390cfb0312ea54b288d39ff0b68357948
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-03-25 18:38:05 +0530

    Added Block class

---
+++ src/Block.ts
@@ -0,0 +1,12 @@
+
+// Imports
+import { Vector } from './utils/Vector'
+import { Colors } from './utils/Colors'
+import {} from './utils/Border'
+import { Plotter } from './utils/Plotter'
+
+// Interface for Block class
+export interface IBlock {}
+
+// Block class
+export class Block {}
