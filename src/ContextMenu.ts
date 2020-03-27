1d29af526fa965957c7b02ecdeb1a1348c2655f6
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-03-27 12:45:59 +0530

    Added comments

--- src/ContextMenu.ts
+++ src/ContextMenu.ts
@@ -16,24 +16,43 @@ export interface IContextMenu {
 // Context Menu class
 export class ContextMenu implements IContextMenu {
 
+	/**
+	 * Class Members
+	 */
+
 	private isShowing: boolean = false
 	private at: Vector | null = null
 	private onBlock: Block | undefined = undefined
 
+	/**
+	 * Constructs a ContextMenu instance
+	 */
 	public constructor() {}
 
+	/**
+	 * Shows the context menu
+	 * @param at The location of the cursor
+	 * @param block If on a block then a reference to that block
+	 */
 	public show(at: Vector, block?: Block) {
 		this.at = at
 		this.onBlock = block
 		this.isShowing = true
 	}
 
+	/**
+	 * Hides the context menu
+	 */
 	public hide(): void {
 		this.isShowing = false
 		this.at = null
 		this.onBlock = undefined
 	}
 
+	/**
+	 * Render the context menu
+	 * @param plotter Plotter for rendering
+	 */
 	public render(plotter: Plotter): void {
 		if (this.isShowing && this.at) {
 			if (this.onBlock) {
