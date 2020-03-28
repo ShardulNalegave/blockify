04401c73f7e3d232384e3814a27ba21e6b9ea795
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-03-28 21:30:10 +0530

    Working on block connections

---
+++ src/BlockConnector.ts
@@ -0,0 +1,12 @@
+
+// Interface for Block Connectors
+export interface IBlockConnector {}
+
+// Block Connector class
+export class BlockConnector implements IBlockConnector {}
+
+// Interface for a Block Connection
+export interface IBlockConnection {}
+
+// Block Connection class
+export class BlockConnection implements IBlockConnection {}
